c502194412432259c7867bf77b82c6eeb4c8f096
Andrew Blackburn <andrew.blackburn@protonmail.com>
2020-06-14 15:26:46 -0400

    Switch to using json for the specs file

--- spec/index.js
+++ spec/index.js
@@ -1,7 +1,7 @@
 
 const path = require('path');
 const OpenApiValidator = require('express-openapi-validator').OpenApiValidator;
-const apiSpec = path.join(__dirname, 'api.yaml');
+const apiSpec = path.join(__dirname, 'openapi.json');
 
 module.exports.init = (app) => {
     return new OpenApiValidator({
