4f8ccaa88ce18258f642a2ec7a5a207a8dfbf0e3
Andrew Blackburn <andrew.blackburn@protonmail.com>
2020-06-14 18:35:39 -0400

    Add support for broken apart schemas

--- spec/builder.js
+++ spec/builder.js
@@ -1,48 +1,60 @@
 const _ = require('lodash');
 const fs = require('fs');
 const path = require('path');
-const info = require('./openapi/info.json');
-const servers = require('./openapi/servers.json');
+
+const SPEC_DIR = './openapi'
+const ROUTE_SPECS = `${SPEC_DIR}/routes/`;
+const COMPONENT_SPECS = `${SPEC_DIR}/components`;
 
 module.exports.buildSpec = () => ({
-  openapi: "3.0.3",
-  info,
-  servers,
-  paths: buildPaths('./openapi/routes/'),
-  components: buildComponents()
+    openapi: "3.0.3",
+    info: require(`${SPEC_DIR}/info.json`),
+    servers: require(`${SPEC_DIR}/servers.json`),
+    paths: buildPaths(ROUTE_SPECS),
+    components: buildComponents()
 });
 
 const buildPaths = (apiDir, paths = {}) => {
     const normalizedPath = path.join(__dirname, apiDir);
     fs.readdirSync(normalizedPath, { withFileTypes: true }).forEach((item) => {
-        
+
         if (item.isFile()) {
             const routeObj = require(apiDir + item.name);
-            const routeName = path.parse(item.name).name; 
+            const routeName = path.parse(item.name).name;
 
             Object.keys(routeObj).forEach((value => {
                 _.set(routeObj[value], 'operationId', routeName);
                 _.set(routeObj[value], 'x-eov-operation-id', routeName);
-                _.set(routeObj[value], 'x-eov-operation-handler', apiToRouteHandler(apiDir) + routeName);
+                _.set(routeObj[value], 'x-eov-operation-handler', apiToRouteHandler(apiDir, routeName));
             }));
 
-            
-            paths[apiToRoute(apiDir) + routeName] = routeObj;
+
+            paths[`${apiToRoutePath(apiDir)}${routeName}`] = routeObj;
         } else {
-            return buildPaths(apiDir+item.name + '/', paths);
+            return buildPaths(`${apiDir}${item.name}/`, paths);
         }
-        
+
     });
     return paths;
 };
 
-const apiToRoute = (apiDir) => apiDir.replace('./openapi/routes/', '/');
-const apiToRouteHandler = (apiDir) => apiDir.replace('./openapi', '..');
+const apiToRoutePath = (apiDir) => apiDir.replace(ROUTE_SPECS, '/');
+const apiToRouteHandler = (apiDir, routeName) => `${apiDir.replace(SPEC_DIR, '..')}${routeName}`;
 
+const buildComponents = () => ({
+    schemas: buildSchemas(`${COMPONENT_SPECS}/schemas/`),
+    securitySchemes: require(`${COMPONENT_SPECS}/securitySchemes.json`)
+});
 
-const buildComponents = () => {
-     //TODO: proper implementation from seperate object definitions
-     return require('./openapi/components.json');
+const buildSchemas = (schemaDir) => {
+    const schemas = {};
+    const normalizedPath = path.join(__dirname, schemaDir);
+    fs.readdirSync(normalizedPath).forEach((item) => {
+        const schemaObj = require(`${schemaDir}${item}`);
+        const schemaName = path.parse(item).name;
+        schemas[schemaName] = schemaObj;
+    });
+    return schemas;
 };
 
 
