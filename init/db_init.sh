05101458f3c1d465ddd4c14356721f020809acc0
michaelplatt07 <michael.platt.07@gmail.com>
2020-09-12 16:34:00 -0400

    Adding some missing initializations for the database that are required to have the app working.

--- init/db_init.sh
+++ init/db_init.sh
@@ -9,7 +9,16 @@ echo "Loading measurements..."
 mongoimport --db devRecipeDb --collection measurements --file ../dumps/measurements_dump.json
 echo "Loaded measurements"
 echo
+echo "Loading categories..."
+mongoimport --db devRecipeDb --collection categories --file ../dumps/categories_dump.json
+echo "Loaded categories"
+echo
 echo "Loading cuisines..."
 mongoimport --db devRecipeDb --collection cuisines --file ../dumps/cuisines_dump.json
+echo "Loaded cuisines"
+echo
+echo "Loading serving sizes..."
+mongoimport --db devRecipeDb --collection serving_sizes --file ../dumps/serving_sizes_dump.json
+echo "Loaded serving sizes"
 echo
 echo "Finished loading dev database."
