39020d05bdfb7de9c1bed3394aed54410fb9e295
michaelplatt07 <michael.platt.07@gmail.com>
2020-09-12 16:38:31 -0400

    Removing duplicate initialization of db

--- init/db_init.sh
+++ init/db_init.sh
@@ -21,7 +21,4 @@ echo "Loading serving sizes..."
 mongoimport --db devRecipeDb --collection serving_sizes --file ../dumps/serving_sizes_dump.json
 echo "Loaded serving sizes"
 echo
-echo "Loading categories..."
-mongoimport --db devRecipeDb --collection categories --file ../dumps/categories_dump.json
-echo
 echo "Finished loading dev database."
